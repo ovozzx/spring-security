8b57e04ae02b48b349bb3a363a7f0a457191e7e7
CodeMakers-KR <codemakers.kr@gmail.com>
2025-09-26 17:08:45 +0900

    이메일 중복검사 완료

--- hello-spring/src/main/resources/static/js/member/regist.js
+++ hello-spring/src/main/resources/static/js/member/regist.js
@@ -9,7 +9,10 @@ $().ready(function() {
     $("#email").on("keyup", function() {
         var value = $(this).val();
         
-        var validateErrorCount = $(this).closest("div").find(".validate-error").length;
+        var validateErrorCount = $(this).closest("div")
+                                        .find(".validate-error")
+                                        .not(".not-ok")
+                                        .length;
         if (validateErrorCount > 0) {
             $(this).closest("div").find("span.not-ok").remove();
             $(this).closest("div").find("span.ok").remove();
@@ -26,6 +29,7 @@ $().ready(function() {
                 var okCount = $(that).closest("div").find("span.ok").length;
                 if (okCount === 0) {
                     $(that).after( $("<span class='ok'>사용 가능한 이메일입니다.</span>") );
+                    autoActive();
                 }
             }
             else {
